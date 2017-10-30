7520a2a7fc8c6634933a25072302e30f2025203a
Thom van Dijk <thom.a.vandijk@gmail.com>
2017-10-30 13:35:33 +0100

    added simple login template on header

--- html/index.php
+++ html/index.php
@@ -10,26 +10,39 @@
 </head>
 <body>
 
-<div class="jumbotron text-center">
-  <h1>FFXIV Market</h1>
-</div>
-
 <div class="container">
-    <div class="row-sm-4">
+  <div class="jumbotron">
+    <div class="row">
+      <div class="col-sm-10">
+        <h1>FFXIV Market</h1>
+      </div>
+      <div class="col-sm-2">
+        <div class="form-group">
+          <p><input type="text" class="form-control" id="usr" value="Username"></p>
+          <p><input type="password" class="form-control" id="pwd" value=""></p>
+          <p><input type="submit" class="form-control" id="pwd" value="Login"></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-12">
+    <div class="row">
       <h3>Column 1</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
-    <div class="row-sm-4">
+    <div class="row">
       <h3>Column 2</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
-    <div class="row-sm-4">
+    <div class="row">
       <h3>Column 3</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
+  </div>
 </div>
 
 </body>
