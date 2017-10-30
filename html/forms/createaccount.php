2825e961216607d9c42aff4184397a5ce41339a1
Jeroen <something@local.nl>
2017-10-30 19:43:06 +0100

    Added new style file for the modals Added style to center the modal Fixed issues with modal markup (bootstrap version issue)

--- html/forms/createaccount.php
+++ html/forms/createaccount.php
@@ -4,8 +4,8 @@
   <title>FFXIV Market</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../style/modal.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="../js/forms.js"></script> <!-- Change this when included in index.php !!!!  -->
@@ -13,47 +13,43 @@
 <body>
 
 
-  <!-- Button trigger modal -->
-  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#createAccount">
-    Create account
-  </button>
+  <!-- Trigger the modal with a button -->
+  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#createAccount">Create account</button>
 
   <!-- Modal -->
   <div class="modal fade" id="createAccount" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
+    <div class="vertical-alignment-helper">
+      <div class="modal-dialog vertical-align-center">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Create account</h4>
+          </div>
+          <div class="modal-body">
+            <form action="#">
+              <div class="form-group" id="usernameInputDiv">
+                <label for="firstname">Desired username:</label>
+                <input type="text" class="form-control" name="firstname" placeholder="username" onfocusout="validateForm()" id="usernameInputField">
+                <span id="usernameFeedback"></span>
+              </div>
 
-        <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+              <label for="password">Password:</label> <br/>
+              <input type="password" class="form-control" name="password" placeholder="password"> <br/>
+
+              <label for="username">Verify password:</label> <br/>
+              <input type="password" class="form-control" name="verpassword" placeholder="password"> <br/>
+              <br/><br/>
+              <button type="submit" class="btn btn-primary" formmethod="post">Submit</button>
+          </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
         </div>
-
-        <div class="modal-body">
-
-          <form action="#">
-            <div class="form-group" id="usernameInputDiv">
-              <label for="firstname">Desired username:</label>
-              <input type="text" class="form-control" name="firstname" placeholder="username" onfocusout="validateForm()" id="usernameInputField">
-              <span id="usernameFeedback"></span>
-            </div>
-
-            <label for="password">Password:</label> <br/>
-            <input type="password" class="form-control" name="password" placeholder="password"> <br/>
-
-            <label for="username">Verify password:</label> <br/>
-            <input type="password" class="form-control" name="verpassword" placeholder="password"> <br/>
-            <br/><br/>
-            <button type="submit" class="btn btn-primary" formmethod="post">Submit</button>
-        </form>
-
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-primary"  data-toggle="modal" data-target="#createAccount">Close</button>
-        </div>
-      </div>
     </div>
+      </div>
   </div>
+
+
 </body>
 </html>
