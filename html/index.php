886789e56779fb837e6442dc56cedb59be49f03a
Jeroen <something@local.nl>
2017-10-30 21:53:38 +0100

    Fixied the modal issue where the modal didn't get destroyed propperly

--- html/index.php
+++ html/index.php
@@ -9,6 +9,18 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="js/forms.js"></script>
+  <script>
+  // This function destroys a modal completly.
+  $(document).ready(function()
+  {
+      // codes works on all bootstrap modal windows in application
+      $('.modal').on('hidden.bs.modal', function(e)
+      {
+          $(this).removeData();
+      }) ;
+
+  });
+</script>
 </head>
 <body>
 
