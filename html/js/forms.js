78f04cafee1843a6e11555f0e5a75084f5cdaa0d
Jeroen <something@local.nl>
2017-10-31 20:55:33 +0100

    Added an check if an field is empty or not

--- html/js/forms.js
+++ html/js/forms.js
@@ -1,47 +1,52 @@
 // All form related JS belongs in this file
 function validateValuesCreateForm() {
 
-  // Get fields for username
-  var createUsernameBox = document.getElementById("createUsernameBox");;
-  var feedbackHelper = document.getElementById("createUsernameHelper");
-  var usernameInput = document.getElementById("createUsernameInput");;
-  var usernameFeedbackIcon = document.getElementById("createUsernameFeedback");
+  var usernameInput = document.getElementById("createUsernameInput");
 
-  // Check username input
-  if (usernameInput.value == "Name") {
-      createUsernameBox.className = "form-group has-success has-feedback"
-      feedbackHelper.style.visibility = "hidden";
-      usernameFeedbackIcon.className = "glyphicon glyphicon-ok form-control-feedback";
-  }else{
-      createUsernameBox.className = "form-group has-error has-feedback"
-      feedbackHelper.style.visibility = "visible";
-      feedbackHelper.innerHTML = "The error message";
-      usernameFeedbackIcon.className = "glyphicon glyphicon-remove form-control-feedback";
+  if(usernameInput.value.length != 0){
+    // Get fields for username
+    var createUsernameBox = document.getElementById("createUsernameBox");;
+    var feedbackHelper = document.getElementById("createUsernameHelper");
+    var usernameFeedbackIcon = document.getElementById("createUsernameFeedback");
+
+    // Check username input
+    if (usernameInput.value == "Name") {
+        createUsernameBox.className = "form-group has-success has-feedback"
+        feedbackHelper.style.visibility = "hidden";
+        usernameFeedbackIcon.className = "glyphicon glyphicon-ok form-control-feedback";
+    }else{
+        createUsernameBox.className = "form-group has-error has-feedback"
+        feedbackHelper.style.visibility = "visible";
+        feedbackHelper.innerHTML = "The error message";
+        usernameFeedbackIcon.className = "glyphicon glyphicon-remove form-control-feedback";
+    }
   }
 
-
-  // Get fields for email
-  var createEmailBox = document.getElementById("createEmailBox");;
-  var emailHelper = document.getElementById("createEmailHelper");
   var emailInput = document.getElementById("createEmailInput");;
-  var emailFeedbackIcon = document.getElementById("createEmailFeedback");
 
-  // email Regex
-  var expression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  var emailGiven = emailInput.value;
-  var regex = new RegExp(expression);
-  var result = regex.test(emailGiven);
+  if(emailInput.value.length != 0){
+    // Get fields for email
+    var createEmailBox = document.getElementById("createEmailBox");;
+    var emailHelper = document.getElementById("createEmailHelper");
+    var emailFeedbackIcon = document.getElementById("createEmailFeedback");
 
-  // Check username input
-  if (result) {
-      createEmailBox.className = "form-group has-success has-feedback"
-      emailHelper.style.visibility = "hidden";
-      emailFeedbackIcon.className = "glyphicon glyphicon-ok form-control-feedback";
-  }else{
-      createEmailBox.className = "form-group has-error has-feedback"
-      emailHelper.style.visibility = "visible";
-      emailHelper.innerHTML = "The error message";
-      emailFeedbackIcon.className = "glyphicon glyphicon-remove form-control-feedback";
+    // email Regex
+    var expression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var emailGiven = emailInput.value;
+    var regex = new RegExp(expression);
+    var result = regex.test(emailGiven);
+
+    // Check username input
+    if (result) {
+        createEmailBox.className = "form-group has-success has-feedback"
+        emailHelper.style.visibility = "hidden";
+        emailFeedbackIcon.className = "glyphicon glyphicon-ok form-control-feedback";
+    }else{
+        createEmailBox.className = "form-group has-error has-feedback"
+        emailHelper.style.visibility = "visible";
+        emailHelper.innerHTML = "The error message";
+        emailFeedbackIcon.className = "glyphicon glyphicon-remove form-control-feedback";
+    }
   }
 }
 
