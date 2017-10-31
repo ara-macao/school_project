f903eb7b35597e8ddb18ab2da6bf3f92ca973217
Jeroen <something@local.nl>
2017-10-31 17:38:03 +0100

    Added spacing so every part of the modal is more clear in the code

--- html/forms/createaccount.php
+++ html/forms/createaccount.php
@@ -2,13 +2,14 @@
   <button type="button" class="close" data-dismiss="modal">&times;</button>
   <h4 class="modal-title">Create account</h4>
 </div>
+
 <div class="modal-body">
   <form action="#">
     <div class="form-group" id="createUsernameBox">
       <label for="firstname">Desired username:</label>
       <!-- Shows detailed information on what went wrong -->
       <small id="createFeedbackHelper" class="text-danger" style="visibility: hidden">
-        
+
       </small>
 
       <!-- input field for username -->
@@ -21,19 +22,17 @@
     <small id="createPasswordHelper" class="text-danger">
       Must be 8-20 characters long and contain A-Z, a-z, 0-9, ! @ # $ % ?
     </small>
-
     <br/>
     <!-- input field for password -->
     <input type="password" class="form-control" name="password" placeholder="password"> <br/>
-
  <br/>
     <label for="username">Verify password:</label> <br/>
     <!-- input field for password -->
     <input type="password" class="form-control" name="verpassword" placeholder="password"> <br/>
     <br/><br/>
-
 </form>
 </div>
+
 <div class="modal-footer">
   <button type="submit" class="btn btn-primary" formmethod="post" style="float: left">Submit</button>
   <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
