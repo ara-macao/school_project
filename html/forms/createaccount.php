335a00431f97db18dbfe35f5396cb6c9dc2212bb
Jeroen <something@local.nl>
2017-10-31 09:40:18 +0100

    Create account form has now text warnings Username textwarning added to js Password textwarning has no functionallity yet

--- html/forms/createaccount.php
+++ html/forms/createaccount.php
@@ -6,17 +6,32 @@
   <form action="#">
     <div class="form-group" id="createUsernameBox">
       <label for="firstname">Desired username:</label>
+      <!-- Shows detailed information on what went wrong -->
+      <small id="createFeedbackHelper" class="text-danger" style="visibility: hidden">
+        
+      </small>
+
+      <!-- input field for username -->
       <input type="text" class="form-control" name="firstname" placeholder="username" onfocusout="validateCreateForm()" id="createUsernameInput">
       <span id="createFeedback"></span>
     </div>
 
-    <label for="password">Password:</label> <br/>
+    <label for="password">Password:</label>
+    <!-- Shows detailed information on what went wrong-->
+    <small id="createPasswordHelper" class="text-danger">
+      Must be 8-20 characters long and contain A-Z, a-z, 0-9, ! @ # $ % ?
+    </small>
+
+    <br/>
+    <!-- input field for password -->
     <input type="password" class="form-control" name="password" placeholder="password"> <br/>
 
+ <br/>
     <label for="username">Verify password:</label> <br/>
+    <!-- input field for password -->
     <input type="password" class="form-control" name="verpassword" placeholder="password"> <br/>
     <br/><br/>
-    
+
 </form>
 </div>
 <div class="modal-footer">
--- html/js/forms.js
+++ html/js/forms.js
@@ -3,13 +3,19 @@ function validateCreateForm() {
   var nameInput = document.getElementById("createUsernameInput");;
   var inputField = document.getElementById("createUsernameBox");;
   var feedbackIcon = document.getElementById("createFeedback");
+  var feedbackHelper = document.getElementById("createFeedbackHelper");
 
     // If the nameinput is equal to Name mark it correct!
     if (nameInput.value == "Name") {
         inputField.className = "form-group has-success has-feedback"
         feedbackIcon.className = "glyphicon glyphicon-ok form-control-feedback";
+        feedbackHelper.style.visibility = "hidden";
     }else{
         inputField.className = "form-group has-error has-feedback"
         feedbackIcon.className = "glyphicon glyphicon-remove form-control-feedback";
+        feedbackHelper.style.visibility = "visible";
+
+
+        feedbackHelper.innerHTML = "The error message";
     }
 }
