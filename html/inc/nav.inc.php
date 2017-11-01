6d36a8eea2dbc18802f1a20348d252ad98a0ab03
Jeroen <something@local.nl>
2017-11-01 21:37:52 +0100

    Added included to the index.php

--- html/inc/nav.inc.php
+++ html/inc/nav.inc.php
@@ -3,7 +3,6 @@
     <div class="navbar-header">
       <img alt="logo" src="https://vignette.wikia.nocookie.net/finalfantasy/images/5/5d/FFXIV_A_Realm_Restored_trophy_icon.png/revision/latest?cb=20160508021814" style=" float: left; width: 50px; heigh: 50px;">
       <h4 class="navbar-text">FFXIV MARKET</h4>
-      <button id="replace" type="button" style="float:right">simulate login</button><br><br>
     </div>
     <div id="navbuttons" class="show">
       <ul class="nav navbar-nav navbar-right">
@@ -17,11 +16,11 @@
           <a class="dropdown-toggle" data-toggle="dropdown" href="#">
           <span class="glyphicon glyphicon-user"></span> <?php echo $user->username; ?> <span class="caret"></span></a>
 
-         <ul class="dropdown-menu">
-           <li><a data-toggle="modal" href="forms/account.php" id="accountpage" data-target="#remoteModal">Account</a></li>
-           <li><a data-toggle="modal" href="forms/characters.php" id="characterspage" data-target="#remoteModal">Characters</a></li>
-          <li><a id="logoutbtn">Logout</a></li>
-         </ul>
+          <ul class="dropdown-menu">
+            <li><a data-toggle="modal" href="forms/account.php" id="accountpage" data-target="#remoteModal">Account</a></li>
+            <li><a data-toggle="modal" href="forms/characters.php" id="characterspage" data-target="#remoteModal">Characters</a></li>
+            <li><a id="logoutbtn">Logout</a></li>
+          </ul>
         </li>
       </ul>
     </div>
