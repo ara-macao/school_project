f9e035470ff37f68402b1c539129d734001ac1bc
madbronine <R.dman93@gmail.com>
2017-11-02 19:17:56 +0100

    refactored serverlist script.

--- html/inc/head.inc.php
+++ html/inc/head.inc.php
@@ -29,45 +29,7 @@ if(array_key_exists('token', $_SESSION)){
       {
           $(this).removeData();
       });
+    serverList();
   });
-  //Server list button generator
-    $.get("./api/?action=getServerList", function(data, status){
-            var result = JSON.parse(data);
-            for (var i = 0; i < result["data"].length; i++) {
-             switch (result["data"][i]["datacenter"]) {
-                case "Mana":
-                    var radioBtn = $('<label><input type="radio" name="optradio"> '+result["data"][i]["server"]+'</input></label><br>');
-              radioBtn.appendTo('#DCMANA');
-                      console.log(result["data"][i]);
-                      break;
-                  case "Gaia":
-                      var radioBtn = $('<label><input type="radio" name="optradio"> ' + result["data"][i]["server"] + '</input></label><br>');
-                      radioBtn.appendTo('#DCGAIA');
-                      console.log(result["data"][i]);
-                      break;
-                  case "Elemental":
-                      var radioBtn = $('<label><input type="radio" name="optradio"> ' + result["data"][i]["server"] + '</input></label><br>');
-                      radioBtn.appendTo('#DCELEMENTAL');
-                      console.log(result["data"][i]);
-                      break;
-                  case "Aether":
-                      var radioBtn = $('<label><input type="radio" name="optradio"> ' + result["data"][i]["server"] + '</input></label><br>');
-                      radioBtn.appendTo('#DCAETHER');
-                      console.log(result["data"][i]);
-                      break;
-                  case "Primal":
-                      var radioBtn = $('<label><input type="radio" name="optradio"> ' + result["data"][i]["server"] + '</input></label><br>');
-                      radioBtn.appendTo('#DCPRIMAL');
-                      console.log(result["data"][i]);
-                      break;
-                  case "Chaos":
-                      var radioBtn = $('<label><input type="radio" name="optradio"> ' + result["data"][i]["server"] + '</input></label><br>');
-                      radioBtn.appendTo('#DCCHAOS');
-                      console.log(result["data"][i]);
-                      break;
-                  default:
-              }
-          }
-      });
   </script>
 </head>
