7ff76a3c8d15f5c096a1708758000eddfcaad8c4
Jeroen <something@local.nl>
2017-11-02 23:39:35 +0100

    Added autocomplete (only comments yet)

---
+++ html/js/autocomplete.js
@@ -0,0 +1,7 @@
+// typt de eerste 3 chararacters -> modulo
+// dan doe je request naar de server met itemsearch met 3 chararacters
+// die genereert dataset die hij teruggeeft
+// dan update je jquery
+
+// database search
+// Onchangeevent kijken naar value (kijk 0 zo niet modulo 3 als die 0 is -> check dan result max 5 of 10)
