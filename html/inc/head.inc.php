b8c7a6894a8c6840f78c3366ea64f96ba6274028
Jeroen <something@local.nl>
2017-11-03 17:03:41 +0100

    Fixed autofill for search field

--- html/inc/head.inc.php
+++ html/inc/head.inc.php
@@ -13,8 +13,10 @@ if(array_key_exists('token', $_SESSION)){
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="style/modal.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="js/forms.js"></script>
   <script src="js/login.js"></script>
