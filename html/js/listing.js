520821d062d07235e93cdc8f6dba188d76737664
Emiel van Essen <emiel@astralvision.nl>
2017-11-03 02:52:46 +0100

    Added listing ID to Query

--- html/js/listing.js
+++ html/js/listing.js
@@ -36,8 +36,9 @@ function refreshCallback(html)
         $("#loginUsernameFeedback").hide();
         var result = JSON.parse(data["data"]);
         for (var i = 0; i < result.length; i++) {
-          var listing = $('<button type="button" class="btn btn-' + (result[i]["listing_type"] == 0 ? 'primary' : 'warning') + ' col-sm-12" data-toggle="modal" href="forms/buyitem.php" id="buyitem" data-target="#remoteModal"> ' + result[i]["item_count"] + " x " + result[i]["item_price"] + " GIL    " + result[i]["item_nicename"] + '</button><br><br>');
+          var listing = $('<button type="button" data-button='{"listingid": "option1", "option2": "option2"}' class="btn btn-' + (result[i]["listing_type"] == 0 ? 'primary' : 'warning') + ' col-sm-12" data-toggle="modal" href="forms/buyitem.php" id="buyitem" data-target="#remoteModal" > ' + result[i]["item_count"] + " x " + result[i]["item_price"] + " GIL    " + result[i]["item_nicename"] + '</button><br><br>');
           listing.appendTo("#" + (result[i]["listing_type"] == 0 ? "sell" : "buy") + "-orders");
+          console.log(result[i]);
         }
     }
 }
