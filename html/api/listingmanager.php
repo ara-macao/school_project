0f4505b4c5f5e07e6fb0ca1f4f516f2928f82250
Emiel van Essen <emiel@astralvision.nl>
2017-11-07 16:48:37 +0100

    adde avatar url to getlistingwithid

--- html/api/listingmanager.php
+++ html/api/listingmanager.php
@@ -134,7 +134,7 @@ public function getListings($buying = "both"/*!< Buying or selling */, $serverid
 
       $PDO = getPDO();
 
-      $sql = "SELECT item.item_nicename, item.item_description, item.item_image_url, listing.item_price, listing.item_count, `character`.character_name, listing.comment, server.server AS server_name, item.lodestone_item_id FROM listing " .
+      $sql = "SELECT item.item_nicename, item.item_description, item.item_image_url, listing.item_price, listing.item_count, `character`.character_name, listing.comment, server.server AS server_name, item.lodestone_item_id, `character`.character_avatar_url FROM listing " .
              "INNER JOIN `character` ON listing.lodestone_character_id = `character`.lodestone_character_id " .
              "INNER JOIN server ON `character`.character_server = server.id " .
              "INNER JOIN item ON listing.item_id = item.item_id " .
