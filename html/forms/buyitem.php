233acc426b36d7aff52f917c8bc78e32b7a33a07
madbronine <R.dman93@gmail.com>
2017-11-07 15:19:12 +0100

    + added ref link for eorzadb_link in picture and header

--- html/forms/buyitem.php
+++ html/forms/buyitem.php
@@ -1,12 +1,12 @@
 <!-- Modal -->
+<script src="https://img.finalfantasyxiv.com/lds/pc/global/js/eorzeadb/loader.js?v2"></script>
 <div class="modal-header" id="inameheader">
   <button type="button" class="close" data-dismiss="modal">&times;</button>
-    <h4 class="modal-title"></h4>
+    <h4 class="modal-title"></h4> 
 </div>
 <div class="modal-body">
     <form action="#">
         <div class="form-group">
-
             <div class="row">
                 <div class="col-sm-4" id="itemimg">
                     <!-- Item picture will be posted here -->
